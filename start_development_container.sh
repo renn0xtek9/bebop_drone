34eb94c16862e8feab795df6a9a4c08794884584
Maxime Haselbauer <renn0xtek9@laposte.net>
2021-07-11 00:48:33 +0200

    Use latest container image for developement

--- start_development_container.sh
+++ start_development_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
-IMAGE=drone/devel-container:v2
+IMAGE=drone/devel-container:v3
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
-docker run --rm -it -v $(pwd):/home/user/src --network=host drone/devel-container:v1 
+docker run --rm -it -v $(pwd):/home/user/src --network=host $IMAGE
 
