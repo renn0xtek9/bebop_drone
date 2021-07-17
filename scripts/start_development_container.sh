86a52ba8b0fa01fad29129778ec67aa0996c14b8
Maxime Haselbauer <renn0xtek9@laposte.net>
2021-07-17 02:11:30 +0200

    now using v4 container

--- scripts/start_development_container.sh
+++ scripts/start_development_container.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/../
 
-IMAGE=drone/devel-container:v3
+IMAGE=drone/devel-container:v4
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 docker run --rm -it -v $(pwd):/home/user/src --network=host $IMAGE
