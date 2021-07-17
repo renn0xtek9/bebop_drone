c3f8f6c42bf6a46015a06b54879a80154458529b
Maxime Haselbauer <renn0xtek9@laposte.net>
2021-07-17 02:11:58 +0200

    Remove unecessary comments

--- src/bebop_keyboard_controller/src/keyboard_hook.cpp
+++ src/bebop_keyboard_controller/src/keyboard_hook.cpp
@@ -7,9 +7,6 @@
 #include <thread>
 #include <vector>
 
-
-#include "./../test/test_keyboard_hook_utils.h"
-
 using namespace std::chrono_literals;
 
 namespace bebop_keyboard_controller {
@@ -23,21 +20,14 @@ void KeyboardHook::run() {
   };
 
   while (allowed_to_run) {
-
-
-    
     std::array<char, 32> key_sequence{};
     XQueryKeymap(display_, key_sequence.data());
-    printf("is pressed:\n");
-    displayArray(key_sequence);
 
     for (auto &key_status : statuses_) {
       if (isPressed(key_sequence.data(), key_status.key_defined)) {
-        printf("key pressed !\n");
         // rising front
         if (!key_status.is_currently_pressed) {
           key_status.is_currently_pressed = true;
-          printf("calling callback !\n");
           key_status.callback();
         }
       } else {
--- src/bebop_keyboard_controller/test/test_keyboard_hook.cpp
+++ src/bebop_keyboard_controller/test/test_keyboard_hook.cpp
@@ -8,7 +8,6 @@
 #include <X11/extensions/XTest.h>
 #include <X11/keysym.h>
 
-#include "test_keyboard_hook_utils.h"
 #include "test_utils.h"
 
 using namespace std::chrono_literals;
@@ -22,75 +21,58 @@ struct CallbackMocks {
   CallbackMock pageUp{};
   CallbackMock pageDown{};
 };
-// CallbackMocks mocks{};
 
 class PressingOnKeys : public ::testing::Test {
 
 protected:
-  KeyboardHook *keyboard_hook;
-  std::thread keyboard_hook_thread{};
-  Display *display{};
-   CallbackMocks mocks_{};
+  KeyboardHook *keyboard_hook_;
+  std::thread keyboard_hook_thread_{};
+  Display *display_{};
+  CallbackMocks mocks_{};
 
-  Display* getDisplay() {
-    if (display == nullptr) {
-      Display *display{};
-      display = XOpenDisplay(":0");
+  Display *getDisplay() {
+    if (display_ == nullptr) {
+      Display *display_{};
+      display_ = XOpenDisplay(":0");
     }
-    return display;
+    return display_;
   }
   virtual void SetUp() {
-    keyboard_hook = new KeyboardHook(mocks_.upArrow(), mocks_.downArrow(),
+    keyboard_hook_ = new KeyboardHook(mocks_.upArrow(), mocks_.downArrow(),
                                      mocks_.leftArrow(), mocks_.rightArrow(),
                                      mocks_.pageDown(), mocks_.pageUp());
-    keyboard_hook_thread = std::thread(
-        &bebop_keyboard_controller::KeyboardHook::run, keyboard_hook);
+    keyboard_hook_thread_ = std::thread(
+        &bebop_keyboard_controller::KeyboardHook::run, keyboard_hook_);
   }
   virtual void terminateHook() {
-    keyboard_hook->stop();
-    keyboard_hook_thread.join();
+    keyboard_hook_->stop();
+    keyboard_hook_thread_.join();
   }
-  virtual void TearDown() { delete keyboard_hook; }
+  virtual void TearDown() { delete keyboard_hook_; }
 };
 
 void pressAndReleaseKey(Display *display, unsigned int keycode) {
   XTestFakeKeyEvent(display, keycode, False, CurrentTime);
   XFlush(display);
-  // displayKeymap(display);
 
   XTestFakeKeyEvent(display, keycode, True, CurrentTime);
   XFlush(display);
-  displayKeymap(display);
 
   XTestFakeKeyEvent(display, keycode, False, CurrentTime);
   XFlush(display);
 }
 
 TEST_F(PressingOnKeys, UpArrow) {
-  // std::thread keyboard_hook_thread(
-  //     &bebop_keyboard_controller::KeyboardHook::run, keyboard_hook);
   ASSERT_EQ(mocks_.upArrow.numberOfCalls(), 0);
-
   unsigned int keycode = XKeysymToKeycode(getDisplay(), XK_Up);
   pressAndReleaseKey(getDisplay(), keycode);
   terminateHook();
-
   EXPECT_EQ(mocks_.upArrow.numberOfCalls(), 1);
 }
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
-  // bebop_keyboard_controller::KeyboardHook keyboard_hook(
-  //     mocks.upArrow(), mocks.downArrow(), mocks.leftArrow(),
-  //     mocks.rightArrow(), mocks.pageDown(), mocks.pageUp());
-
-  // std::thread keyboard_hook_thread(
-  //     &bebop_keyboard_controller::KeyboardHook::run, &keyboard_hook);
-
   //   ros::init(argc, argv, "tester");
   //   ros::NodeHandle nh;
-  int ret = RUN_ALL_TESTS();
-  // keyboard_hook.stop();
-  // keyboard_hook_thread.join();
-  return ret;
+  return RUN_ALL_TESTS();
 }
