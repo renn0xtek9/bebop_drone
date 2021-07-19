bafda4649a61ad4846c5baa75571072b3b164da9
Maxime Haselbauer <renn0xtek9@laposte.net>
2021-07-19 21:33:10 +0200

    Fix script that starts container

--- scripts/start_development_container.sh
+++ scripts/start_development_container.sh
@@ -4,6 +4,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/../
 
 IMAGE=drone/devel-container:v4
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SCRIPT_DIR
 docker run --rm -it -v $(pwd):/home/user/src --network=host $IMAGE
