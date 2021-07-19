6cfc28c917a9566d967737491f4ab5ab943af85b
Maxime Haselbauer <renn0xtek9@laposte.net>
2021-07-19 23:31:21 +0200

    clean a bit the code

--- src/bebop_keyboard_controller/src/bebop_controller.cpp
+++ src/bebop_keyboard_controller/src/bebop_controller.cpp
@@ -2,17 +2,15 @@
 
 namespace bebop_keyboard_controller {
 BebopController::BebopController(const callback &take_off_or_land)
-    : take_off_or_land_{take_off_or_land} 
-    {
-        // take_off_or_land_=std::bind(take_off_or_land);
+    : take_off_or_land_{take_off_or_land} {}
 
-    }
 void BebopController::land() {
   if (is_supposed_to_be_airborne) {
     take_off_or_land_();
     is_supposed_to_be_airborne = false;
   }
 }
+
 void BebopController::takeOff() {
   if (not is_supposed_to_be_airborne) {
     take_off_or_land_();
